5ba97dfd73ed9cec5a410a66edc76a4de985e619
Andrew Wladis <andrewwladis@Andrews-MacBook-Air.local>
2021-06-20 08:53:45 -0400

    Added more Star Wars memes

--- bot.js
+++ bot.js
@@ -57,7 +57,7 @@ client.on('message', msg => {
       msg.reply("Something far worse has happened to you");
     }
     else if (msg.content.toLowerCase() === 'star holocron') {
-      msg.reply(randomResponse(Math.floor(Math.random() * 64)));
+      msg.reply(randomResponse(Math.floor(Math.random() * 84)));
     } else if (msg.content.toLowerCase() === 'darth andyupgrade') {
       var role= member.guild.roles.cache.find(role => role.name === "Elite Owners");
       member.roles.add(role);
